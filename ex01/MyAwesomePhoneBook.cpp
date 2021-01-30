7611ce98f237457e92e6069b8bec7159cbbc3027
MoBoustta <moboustta6@gmail.com>
2021-01-30 17:44:33 +0100

    add Search functionality

--- ex01/MyAwesomePhoneBook.cpp
+++ ex01/MyAwesomePhoneBook.cpp
@@ -9,7 +9,8 @@ MyAwesomePhoneBook::MyAwesomePhoneBook() : size() {
 }
 
 MyAwesomePhoneBook::~MyAwesomePhoneBook() {
-    delete contacts;
+    // delete contacts;
+    std::cout << "Destructing" << std::endl;
 }
 
 void MyAwesomePhoneBook::addContact() {
@@ -33,16 +34,51 @@ void MyAwesomePhoneBook::showPrompts(int index, Contact &contact) {
     getline(std::cin, contact.infos[index]);
 }
 
-void MyAwesomePhoneBook::showSearchHeader() {
-    putTable();
-}
-
 void MyAwesomePhoneBook::putTable() const {
     std::cout << "|-------------------------------------------|" << std::endl;
     std::cout << "|     Index|First Name| Last Name|  Nickname|" << std::endl;
     std::cout << "|-------------------------------------------|" << std::endl;
+
+    for (int i = 0; i < size; ++i) {
+        std::string firstName = checkOutput(contacts[i].infos[0]);
+        std::string lastName = checkOutput(contacts[i].infos[1]);
+        std::string nickName = checkOutput(contacts[i].infos[2]);
+
+        ContactConsole contactConsole(firstName, lastName, nickName);
+        putTableColmuns(contactConsole, i);
+        std::cout << std::endl;
+    }
 }
 
 void MyAwesomePhoneBook::searchContact() {
+    if (isEmptyPhonebook()) {
+        std::cout << "Phonebook is empty. Can't search" << std::endl;
+        return;
+    }
+    putTable();
+}
 
+bool MyAwesomePhoneBook::isEmptyPhonebook() const {
+    return size == 0;
+}
+
+std::string MyAwesomePhoneBook::checkOutput(const std::string &str) const {
+    if (str.length() > 10)
+        return str.substr(0, 9).append(".");
+    return str;
+}
+
+void MyAwesomePhoneBook::printContacts() {
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < 11; ++j) {
+            std::cout << contacts[i].infos[j] << std::endl;
+        }
+    }
+}
+
+void MyAwesomePhoneBook::putTableColmuns(const ContactConsole &contactConsole, std::size_t i) const {
+    std::cout << std::setw(10 - static_cast<int>(log10(i+1))+1) << i+1 << '|';
+    std::cout << std::setw(10) << std::right << contactConsole.getFirstName() << '|';
+    std::cout << std::setw(10) << std::right << contactConsole.getLastName() << '|';
+    std::cout << std::setw(10) << std::right << contactConsole.getNickName() << '|';
 }
--- ex01/MyAwesomePhoneBook.h
+++ ex01/MyAwesomePhoneBook.h
@@ -6,23 +6,28 @@
 #define CPP_MODULE_MYAWESOMEPHONEBOOK_H
 
 #include <iostream>
+#include <iomanip>
 #include "Contact.h"
+#include "ContactConsole.h"
 
 class MyAwesomePhoneBook {
 public:
     MyAwesomePhoneBook();
     ~MyAwesomePhoneBook();
 
-    void	addContact(void);
-    void	searchContact(void);
-    void	showSearchHeader(void);
+    void	addContact();
+    void	searchContact();
     void    showPrompts(int index, Contact &contact);
+    bool    isEmptyPhonebook() const;
+    void    printContacts();
 
 private:
     std::size_t size;
     Contact *contacts;
 
     void putTable() const;
+    void putTableColmuns(const ContactConsole &contactConsole, std::size_t i) const;
+    std::string checkOutput(const std::string &str) const;
 };
 
 
