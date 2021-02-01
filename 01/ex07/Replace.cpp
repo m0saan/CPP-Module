ada7af7a79c9552ffebe26e4ffb0bfe68a31e984
MoBoustta <moboustta6@gmail.com>
2021-02-01 23:44:24 +0100

    start coding ex07 of the Day01 of the Cpp piscine

--- 01/ex07/Replace.cpp
+++ 01/ex07/Replace.cpp
@@ -3,3 +3,22 @@
 //
 
 #include "Replace.h"
+
+Replace::Replace(const std::string &pFilename, const std::string &pS1, const std::string &pS2)
+    : filename(pFilename), s1(pS1), s2(pS2) { }
+
+Replace::~Replace() {
+    std::cout << "Replace's Destructor has been called!" << std::endl;
+}
+
+void Replace::replace() {
+    std::ifstream ifs(filename);
+    std::stringstream ss;
+    std::string line;
+    while (getline(ifs, line))
+        processLine(line);
+}
+
+void Replace::processLine(std::string &line) {
+    std::cout << line << std::endl;
+}
--- 01/ex07/Replace.h
+++ 01/ex07/Replace.h
@@ -5,12 +5,22 @@
 #ifndef CPP_MODULE_REPLACE_H
 #define CPP_MODULE_REPLACE_H
 
-
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
 
 class Replace {
-
+public:
+    Replace(const std::string &filename, const std::string &s1, const std::string & s2);
+    ~Replace();
+    void replace();
+    void processLine(std::string &line);
+private:
+    std::string s1;
+    std::string s2;
+    std::string filename;
 };
-
 
 
 #endif //CPP_MODULE_REPLACE_H
