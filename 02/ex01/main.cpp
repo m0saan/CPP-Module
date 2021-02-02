c575ded06db4a9bbab5bdb6d94b84f42982c67b6
MoBoustta <moboustta6@gmail.com>
2021-02-02 17:13:59 +0100

    coding ex02 of the Day02 of the Cpp piscine

---
+++ 02/ex01/main.cpp
@@ -0,0 +1,26 @@
+//
+// Created by moboustt on 2/2/2021.
+//
+
+#include <iostream>
+#include "Fixed.h"
+
+int main(){
+    Fixed a;
+    Fixed const b( 10 );
+    Fixed const c( 42.42f );
+    Fixed const d( b );
+    a = Fixed( 1234.4321f );
+
+    std::cout << "a is " << a << std::endl;
+    std::cout << "b is " << b << std::endl;
+    std::cout << "c is " << c << std::endl;
+    std::cout << "d is " << d << std::endl;
+
+
+    std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+    std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+    std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+    std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+    return 0;
+}
