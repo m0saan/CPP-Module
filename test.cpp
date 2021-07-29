e1e131cdc168fce13390bd10e74a5d2cf2c670e8
mo-san99 <moboustta6@gmail.com>
2021-07-29 18:25:50 +0100

    playing arround with cpp

--- test.cpp
+++ test.cpp
@@ -1,37 +1,8 @@
-//
-// Created by Mohammad  Boustta on 7/27/21.
-//
-
 #include <iostream>
-#include <cassert>
-#include <cstddef>
-
-class Simple
-{
-private:
-	int m_id;
-
-public:
-	Simple(int id)
-			: m_id{ id }
-	{
-	}
-
-	void setID(int id) { m_id = id; }
-	int getID() { return m_id; }
-};
 
 int main()
 {
-	Simple simple{1};
-	simple.setID(2);
-	std::cout << simple.getID() << '\n';
+	int* value ( new (std::nothrow) int ); // value will be set to a null pointer if the integer allocation fails
 
 	return 0;
 }
-
-/*
- * simple.setID(2) -> setID(&simple, 2)
- * void setID(int id) { m_id = id; } -> void setID(Simple * const this, int id) { this->m_id = id; }
- *
- */
