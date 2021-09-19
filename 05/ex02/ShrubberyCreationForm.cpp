d48083b643e5f411d4759a98fb06c7e4b443a59d
mo-san99 <moboustta6@gmail.com>
2021-09-19 15:21:16 +0100

    CPP d05

--- 05/ex02/ShrubberyCreationForm.cpp
+++ 05/ex02/ShrubberyCreationForm.cpp
@@ -4,17 +4,16 @@
 
 #include "ShrubberyCreationForm.hpp"
 
-ShrubberyCreationForm::ShrubberyCreationForm(std::string const & target)
-	: Form(target, 137, 145) {}
+ShrubberyCreationForm::ShrubberyCreationForm(std::string const &target)
+		: Form(target, 137, 145) {}
 
-ShrubberyCreationForm::~ShrubberyCreationForm() { }
+ShrubberyCreationForm::~ShrubberyCreationForm() {}
 
 void ShrubberyCreationForm::execute(const Bureaucrat &executor) const {
-	if (this->isIsSigned() && executor.getGrade() <= this->getGradeToExec())
-	{
+	if (this->isIsSigned() && executor.getGrade() <= this->getGradeToExec()) {
 		std::fstream f;
-		f.open(getName() + "_shrubbery");
-		if (f.good()){
+		f.open(getName() + "_shrubbery", std::ios::in | std::ios::out | std::ios::trunc);
+		if (f.is_open()) {
 			std::string tree = "    oxoxoo    ooxoo\n"
 							   "  ooxoxo oo  oxoxooo\n"
 							   " oooo xxoxoo ooo ooox\n"
@@ -29,8 +28,9 @@ void ShrubberyCreationForm::execute(const Bureaucrat &executor) const {
 							   "         |  |\n"
 							   "  ______/____\\____";
 			f.write(tree.c_str(), tree.length());
+			f.close();
 		} else
 			std::cout << "Cannot open the file: " << getName() + "_shrubbery" << std::endl;
 	} else
-		throw GradeTooLowException ("Grade too low.");
+		throw GradeTooLowException("Grade too low.");
 }
--- 05/ex02/main.cpp
+++ 05/ex02/main.cpp
@@ -3,6 +3,9 @@
 //
 
 #include "Bureaucrat.hpp"
+#include "PresidentialPardonForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "ShrubberyCreationForm.hpp"
 #include "Form.hpp"
 
 # define BLUE "\e[0;34m"
@@ -12,16 +15,55 @@
 # define RESET "\e[0m"
 
 
-int main(void)
-{
-	Form ff("", 3, 2);
-	Bureaucrat ss("haha", 1);
-	try
-	{
-		ss.incrementGrade();
-		std::cout << ff << std::endl;
-	} catch (std::exception &e)
-	{
-		std::cout << RED << "Exception caught: "<< e.what() << RESET <<std::endl;
+int main(void) {
+
+	std::cout << RED << "*------------------Testing the ShrubberyCreationForm------------------*" << RESET << std::endl;
+	Bureaucrat bureaucrat1("haha", 1);
+	Bureaucrat bureaucrat2("braiiz", 10);
+	ShrubberyCreationForm scf("Home");
+	std::cout << "name: " << scf.getName() << std::endl;
+	std::cout << "GradeToExec: " << scf.getGradeToExec() << std::endl;
+	std::cout << "GradeToSign: " << scf.getGradeToSign() << std::endl;
+	std::cout << std::endl;
+
+	try {
+		scf.execute(bureaucrat1);
+	} catch (std::exception &e) {
+		std::cout << GREEN << e.what() << RESET << std::endl;
 	}
+	scf.beSigned(bureaucrat2);
+	scf.execute(bureaucrat2);
+	std::cout << std::endl;
+
+	std::cout << RED << "*------------------Testing the RobotomyRequestForm------------------*" << RESET << std::endl;
+	RobotomyRequestForm rrf("homeAgain");
+	std::cout << "name: " << rrf.getName() << std::endl;
+	std::cout << "GradeToExec: " << rrf.getGradeToExec() << std::endl;
+	std::cout << "GradeToSign: " << rrf.getGradeToSign() << std::endl;
+	std::cout << std::endl;
+
+	try {
+		rrf.execute(bureaucrat1);
+	} catch (std::exception &e) {
+		std::cout << GREEN << e.what() << RESET << std::endl;
+	}
+	rrf.beSigned(bureaucrat2);
+	rrf.execute(bureaucrat2);
+	std::cout << std::endl;
+
+	std::cout << RED << "*------------------Testing the PresidentialPardonForm------------------*" << RESET << std::endl;
+	Bureaucrat bureaucrat3("haha", 4);
+	PresidentialPardonForm ppf("homeAgain");
+	std::cout << "name: " << ppf.getName() << std::endl;
+	std::cout << "GradeToExec: " << ppf.getGradeToExec() << std::endl;
+	std::cout << "GradeToSign: " << ppf.getGradeToSign() << std::endl;
+	std::cout << std::endl;
+
+	try {
+		ppf.execute(bureaucrat3);
+	} catch (std::exception &e) {
+		std::cout << GREEN << e.what() << RESET << std::endl;
+	}
+	ppf.beSigned(bureaucrat3);
+	ppf.execute(bureaucrat3);
 }
