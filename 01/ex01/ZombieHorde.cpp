cb4722edb255a39d6383952ccad083b0d8be87cf
jsiona <jetigeenn@gmail.com>
2021-11-29 08:54:45 +0300

    fix leaks in zombie horde

--- 01/ex01/ZombieHorde.cpp
+++ 01/ex01/ZombieHorde.cpp
@@ -5,9 +5,8 @@ Zombie* zombieHorde( int N, std::string name ) {
 		return NULL;
     Zombie *Horde = new Zombie[N];
     for (int i = 0; i < N; ++i) {
-		std::string tmpName = name + "_" + std::to_string(i+1);
-        Zombie *z = new Zombie(tmpName);
-        Horde[i] = *z;
+	    std::string tmpName = name + "_" + std::to_string(i+1);
+	    Horde[i] = Zombie(tmpName);
     }
     return Horde;
 }
